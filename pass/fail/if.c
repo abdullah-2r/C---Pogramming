7f2c2906c4a4f5f58865b4918ffad46408cd9e1d
abdullah-2r <abdullahalnaeem37@gmail.com>
2025-07-14 02:00:20 +0600

    nfs

---
+++ pass/fail/if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int mark;
+    scanf("%d",&mark);
+    if(mark>=33){
+        printf("Passed");
+    }
+    else{
+        printf("Failed");
+    }
+    return 0;
+}
